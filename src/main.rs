81c969eae146f189887b3b9c4b796e52b9e656c7
Calbabreaker <calbabreaker@gmail.com>
2021-05-28 16:53:43 +0930

    start of opcodes

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,16 @@ mod cpu;
 use crate::cpu::CPU;
 
 fn main() {
-    let cpu = CPU::new();
-    print!("Bus read 123: {}", cpu.bus.read_byte(123));
+    let mut cpu = CPU::new();
+    cpu.bus.write_byte(0x0000, 0xa9);
+    cpu.bus.write_byte(0x0001, 0x0a);
+    cpu.bus.write_byte(0x0002, 0x81);
+    cpu.bus.write_byte(0x0003, 0x00);
+    cpu.reset();
+
+    for i in 1..15 {
+        cpu.clock();
+    }
+
+    println!("Read: 0x{:x}", cpu.bus.read_byte(0x0aa9));
 }
